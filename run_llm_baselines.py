9055015248fde47cf694e9b56a262b22ee8d4829
Karina Helen Halevy <khalevy@babel-login-4.lti.cs.cmu.edu>
2024-04-16 01:26:42 -0400

    better work on 1 gpu

--- run_llm_baselines.py
+++ run_llm_baselines.py
@@ -160,6 +160,7 @@ def main(args=None):
         model = AutoModelForCausalLM.from_pretrained(args["model_name_or_path"],
                                                      device_map="auto")
         tok = AutoTokenizer.from_pretrained(args["model_name_or_path"])
+        tok.pad_token_id = tok.eos_token_id
     else:
         model = Qwen2ForCausalLM.from_pretrained(args["model_name_or_path"])
         tok = AutoTokenizer.from_pretrained(args["model_name_or_path"])
--- test_llm.sh
+++ test_llm.sh
@@ -4,7 +4,7 @@
 #SBATCH --error=anlp.err
 #SBATCH --partition=long
 #SBATCH --nodes=1
-#SBATCH --gres=gpu:8
+#SBATCH --gres=gpu:1
 #SBATCH --ntasks-per-node=1
 #SBATCH --mem=80GB
 #SBATCH --cpus-per-task=16
