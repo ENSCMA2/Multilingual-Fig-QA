1ea739ee6cf7d36d9e22730af58ec4df8c629a63
lómenoirë mortecc. <38693485+chaosarium@users.noreply.github.com>
2024-04-20 18:23:41 -0400

    cleaning

--- cultural-finetune/finetune.py
+++ cultural-finetune/finetune.py
@@ -1,4 +1,6 @@
 from datasets import load_dataset
+from datasets.dataset_dict import IterableDatasetDict
+from datasets.iterable_dataset import IterableDataset
 import spacy
 from transformers import AutoTokenizer
 from transformers import AutoModelForSequenceClassification
@@ -18,11 +20,20 @@ from transformers import default_data_collator
 import collections
 from transformers import TrainingArguments
 from transformers import Trainer
+from accelerate import Accelerator
+accelerator = Accelerator(cpu=True)
+from itertools import chain
+from dataclasses import dataclass
+from transformers.utils import PaddingStrategy, get_full_repo_name
+import random
+from tqdm import tqdm
+import os, json
+
 
 args = {
     'cultural_corpus': 'yo-bm25-10000',
-    # 'pretrained_model': 'FacebookAI/xlm-roberta-base', 
-    'pretrained_model': 'distilbert-base-uncased', 
+    'pretrained_model': 'FacebookAI/xlm-roberta-base', 
+    # 'pretrained_model': 'distilbert-base-uncased', 
     'corpus_chunk_size': 128,
     'wwm_probability': 0.15,
     'batch_size': 64,
@@ -46,8 +57,11 @@ def mk_tokenizer(args):
     return tokenizer
 
 def mk_models(args):
-    mlm_model = AutoModelForMaskedLM.from_pretrained("FacebookAI/xlm-roberta-base")
-    mc_model = AutoModelForMultipleChoice.from_pretrained("FacebookAI/xlm-roberta-base")
+    config = AutoConfig.from_pretrained(args['pretrained_model'])
+    config.output_hidden_states = True
+    print(f"model config: {config}")
+    mlm_model = AutoModelForMaskedLM.from_pretrained(args['pretrained_model'], config=config)
+    mc_model = AutoModelForMultipleChoice.from_pretrained(args['pretrained_model'], config=config)
     mlm_model.to('cpu')
     mc_model.to('cpu')
     print(f'mlm_model num param: {mlm_model.num_parameters()}')
@@ -128,7 +142,126 @@ def mk_corpus(args, tokenizer):
     
     print("index 0 train example:")
     print(lm_corpus["train"][0])
+    print('decoded:', tokenizer.decode(lm_corpus['train'][0]['input_ids']))
     corpus_mask_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm_probability=0.15)
     corpus_wwm_collator = whole_word_masking_data_collator
 
     return lm_corpus, corpus_mask_collator, corpus_wwm_collator
+
+@dataclass
+class DataCollatorForMultipleChoice: # from multilingual figqa
+    """
+    Data collator that will dynamically pad the inputs for multiple choice received.
+    Args:
+        tokenizer ([`PreTrainedTokenizer`] or [`PreTrainedTokenizerFast`]):
+            The tokenizer used for encoding the data.
+        padding (`bool`, `str` or [`~utils.PaddingStrategy`], *optional*, defaults to `True`):
+            Select a strategy to pad the returned sequences (according to the model's padding side and padding index)
+            among:
+            - `True` or `'longest'`: Pad to the longest sequence in the batch (or no padding if only a single sequence
+              if provided).
+            - `'max_length'`: Pad to a maximum length specified with the argument `max_length` or to the maximum
+              acceptable input length for the model if that argument is not provided.
+            - `False` or `'do_not_pad'` (default): No padding (i.e., can output a batch with sequences of different
+              lengths).
+        max_length (`int`, *optional*):
+            Maximum length of the returned list and optionally padding length (see above).
+        pad_to_multiple_of (`int`, *optional*):
+            If set will pad the sequence to a multiple of the provided value.
+            This is especially useful to enable the use of Tensor Cores on NVIDIA hardware with compute capability >=
+            7.5 (Volta).
+    """
+
+    tokenizer: PreTrainedTokenizerBase
+    padding = True # Union[bool, str, PaddingStrategy]
+    max_length = None # Optional[int]
+    pad_to_multiple_of = None # Optional[int]
+
+    def __call__(self, features):
+        label_name = "label" if "label" in features[0].keys() else "labels"
+        labels = [feature.pop(label_name) for feature in features]
+        batch_size = len(features)
+        num_choices = len(features[0]["input_ids"])
+        flattened_features = [
+            [{k: v[i] for k, v in feature.items()} for i in range(num_choices)] for feature in features
+        ]
+        flattened_features = list(chain(*flattened_features))
+
+        batch = self.tokenizer.pad(
+            flattened_features,
+            padding=self.padding,
+            max_length=self.max_length,
+            pad_to_multiple_of=self.pad_to_multiple_of,
+            return_tensors="pt",
+        )
+
+        # Un-flatten
+        batch = {k: v.view(batch_size, num_choices, -1) for k, v in batch.items()}
+        # Add back labels
+        batch["labels"] = torch.tensor(labels, dtype=torch.int64)
+        return batch
+
+
+def mk_figqa_dataset(args, tokenizer):
+    data_files = {
+        'train': '../langdata/en_train.csv',
+        'validation': '../langdata/en_dev.csv',
+        'test': '../langdata/su.csv',
+    }
+    raw_datasets = load_dataset('csv', data_files=data_files)
+    
+    def preprocess_function(examples):
+        column_names = ['startphrase', 'ending1', 'ending2', 'labels']
+        ending_names = [f"ending{i}" for i in [1, 2]]
+        context_name = "startphrase"
+        label_column_name = "label" if "label" in column_names else "labels"
+
+        first_sentences = [[context] * 2 for context in examples[context_name]]
+        second_sentences = [[examples[end][i] for end in ending_names] for i in range(len(examples[context_name]))]
+        labels = examples[label_column_name]
+
+        # Flatten out
+        first_sentences = list(chain(*first_sentences))
+        second_sentences = list(chain(*second_sentences))
+
+        # Tokenize
+        tokenized_examples = tokenizer(
+            first_sentences,
+            second_sentences,
+            max_length=128,
+            padding=False,
+            truncation=True,
+        )
+
+        # Save the decoded sentences if storing embeddings
+        # if args["do_predict"] and args["save_embeddings"]:
+        #     sentence_fp = os.path.join(args["embedding_output_dir"], "sentences.tsv")
+        #     with open(sentence_fp, "a") as f:
+        #         for i in range(len(tokenized_examples["input_ids"])):
+        #             f.write(tokenizer.decode(tokenized_examples["input_ids"][i]) + "\n")
+
+        # Un-flatten
+        tokenized_inputs = {k: [v[i : i + 2] for i in range(0, len(v), 2)] for k, v in tokenized_examples.items()}
+        tokenized_inputs["labels"] = labels
+
+        return tokenized_inputs
+    
+    processed_figqa_datasets = raw_datasets.map(
+        preprocess_function, batched=True, remove_columns=raw_datasets['train'].column_names
+    )
+    processed_figqa_datasets
+    
+    print('figqa data looks like:')
+    print('train:', processed_figqa_datasets['train'][0])
+    print('decoded:', list(map(tokenizer.decode, processed_figqa_datasets['train'][0]['input_ids'])))
+    print('dev:', processed_figqa_datasets['validation'][0])
+    print('decoded:', list(map(tokenizer.decode, processed_figqa_datasets['validation'][0]['input_ids'])))
+    print('test:', processed_figqa_datasets['test'][0])
+    print('decoded:', list(map(tokenizer.decode, processed_figqa_datasets['test'][0]['input_ids'])))
+    
+    figqa_data_collator = DataCollatorForMultipleChoice(
+        tokenizer, 
+        # pad_to_multiple_of=(8 if accelerator.use_fp16 else None)
+    )
+    
+    return processed_figqa_datasets, figqa_data_collator
