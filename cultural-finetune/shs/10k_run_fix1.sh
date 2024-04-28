af6517a5097d3643b571ff16b385a76202a542f9
lómenoirë mortecc. <38693485+chaosarium@users.noreply.github.com>
2024-04-28 18:17:55 -0400

    fix

---
+++ cultural-finetune/shs/10k_run_fix1.sh
@@ -0,0 +1,11 @@
+# figqa finetue experiments
+## yo 10k
+python main.py --tags report1 --pretrained_model xlm-roberta-base --cultural_corpus yo-bm25-10000 --lang yo --num_corpus_epochs 0 --num_interleaved_epochs 0 --num_figqa_epochs 120 --mc_loss_weight 1.0 --figqa_lr 5e-4 --batch_size 64 --steps_per_interleaved_epoch 32  --corpus_truncate 500 --corpus_chunk_size 128 --gradient_accumulation_steps 8 --finetuned_model yo-2-0.005-10000-4106092417-lora
+## yo 10k
+python main.py --tags report1 --pretrained_model xlm-roberta-base --cultural_corpus yo-bm25-10000 --lang yo --num_corpus_epochs 0 --num_interleaved_epochs 0 --num_figqa_epochs 120 --mc_loss_weight 1.0 --figqa_lr 5e-4 --batch_size 64 --steps_per_interleaved_epoch 32  --corpus_truncate 500 --corpus_chunk_size 128 --gradient_accumulation_steps 8 --finetuned_model yo-2-0.005-10000-4106092417-lora
+## yo 10k
+python main.py --tags report1 --pretrained_model xlm-roberta-base --cultural_corpus yo-bm25-10000 --lang yo --num_corpus_epochs 0 --num_interleaved_epochs 0 --num_figqa_epochs 120 --mc_loss_weight 1.0 --figqa_lr 5e-4 --batch_size 64 --steps_per_interleaved_epoch 32  --corpus_truncate 500 --corpus_chunk_size 128 --gradient_accumulation_steps 8 --finetuned_model yo-2-0.005-10000-4106092417-lora
+## yo 10k
+python main.py --tags report1 --pretrained_model xlm-roberta-base --cultural_corpus yo-bm25-10000 --lang yo --num_corpus_epochs 0 --num_interleaved_epochs 0 --num_figqa_epochs 120 --mc_loss_weight 1.0 --figqa_lr 5e-4 --batch_size 64 --steps_per_interleaved_epoch 32  --corpus_truncate 500 --corpus_chunk_size 128 --gradient_accumulation_steps 8 --finetuned_model yo-2-0.005-10000-4106092417-lora
+## yo 10k
+python main.py --tags report1 --pretrained_model xlm-roberta-base --cultural_corpus yo-bm25-10000 --lang yo --num_corpus_epochs 0 --num_interleaved_epochs 0 --num_figqa_epochs 120 --mc_loss_weight 1.0 --figqa_lr 5e-4 --batch_size 64 --steps_per_interleaved_epoch 32  --corpus_truncate 500 --corpus_chunk_size 128 --gradient_accumulation_steps 8 --finetuned_model yo-2-0.005-10000-4106092417-lora
